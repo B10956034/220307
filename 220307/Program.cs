bccc1e2c4f96aa0e28af06dcee08d0ae73b7d16e
B10956034 <leafrost0312@gmail.com>
2022-03-07 11:54:57 +0800

    done

--- 220307/Program.cs
+++ 220307/Program.cs
@@ -7,8 +7,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
-            string test = "test";
+            //Console.WriteLine("Hello World!");
+
+            //string input = Console.ReadLine();
+
+            //Console.WriteLine("輸入的文字為：" + input);
+
+
+
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            int total = (int)meal_cost + tip_percent + tax_percent;
+
+            Console.WriteLine("總餐費=" + total);
+
+
         }
     }
 }
